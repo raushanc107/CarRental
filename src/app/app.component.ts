3150d40b89ce6df99e810aad9ce454cd695a088f
rizju <rizwaanism@gmail.com>
2025-02-18 16:17:57 +0530

    Second Commit with enhancement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { CarListComponent } from "./components/car-list/car-list.component";
-import { FooterComponent } from "./footer/footer.component";
+import { CarListComponent } from './components/car-list/car-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'CarRental';
-
-
-  multiSlideConfig = {
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    infinite: true,
-  };
-
-  multiSlideImages = [
-    { img: 'https://revvselfdrivecar.s3-us-west-2.amazonaws.com/staging_images/subscriptions_banners_04-min.jpg' },
-    { img: 'https://revvselfdrivecar.s3-us-west-2.amazonaws.com/staging_images/subscriptions_banners_04-min.jpg' },
-    { img: 'https://revvselfdrivecar.s3-us-west-2.amazonaws.com/staging_images/subscriptions_banners_04-min.jpg' },
-    { img: 'https://revvselfdrivecar.s3-us-west-2.amazonaws.com/staging_images/subscriptions_banners_04-min.jpg' },
-    { img: 'https://revvselfdrivecar.s3-us-west-2.amazonaws.com/staging_images/subscriptions_banners_04-min.jpg' },
-    { img: 'https://revvselfdrivecar.s3-us-west-2.amazonaws.com/staging_images/subscriptions_banners_04-min.jpg' }
-  ];
 }
