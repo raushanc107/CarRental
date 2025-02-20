36b19397936351a124ad5723308479936a367163
Raushan chaudhary <raushan.chaudhary@MPL-6KJ0DS3>
2025-02-20 13:55:51 +0530

    minor fix

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -3,12 +3,14 @@ import { map, tap } from 'rxjs';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('Request Intercepted', req);
-  const token = localStorage.getItem('token');
-  const updatedreq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return next(updatedreq);
+  if (req.url.includes('/api/Booking')) {
+    const token = localStorage.getItem('token');
+    const updatedreq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next(updatedreq);
+  }
+  return next(req);
 };
